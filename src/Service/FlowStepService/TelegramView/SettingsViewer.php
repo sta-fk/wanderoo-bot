9a49b3702c44205a75a559a977d33d65a7cf8f0a
Anastasiia <an.winx06@yahoo.com>
2025-06-22 17:46:20 +0300

    WB-23: [fixes] add migration

--- src/Service/FlowStepService/TelegramView/SettingsViewer.php
+++ src/Service/FlowStepService/TelegramView/SettingsViewer.php
@@ -4,6 +4,7 @@ namespace App\Service\FlowStepService\TelegramView;
 
 use App\DTO\Internal\AddStopViewData;
 use App\DTO\Internal\MessageViewIdentifier;
+use App\DTO\Internal\SettingsViewData;
 use App\DTO\Internal\ViewDataInterface;
 use App\DTO\TelegramMessageResponse\SendMessageContext;
 use App\DTO\TelegramMessageResponse\TelegramMessageInterface;
@@ -25,7 +26,7 @@ final readonly class SettingsViewer implements TelegramViewerInterface
 
     public function render(ViewDataInterface $data): TelegramMessageInterface
     {
-        assert($data instanceof AddStopViewData);
+        assert($data instanceof SettingsViewData);
 
         $keyboard = [
             [['text' => $this->translator->trans('menu.keyboard.settings.default_currency'), 'callback_data' => CallbackQueryData::DefaultCurrency->value]],
